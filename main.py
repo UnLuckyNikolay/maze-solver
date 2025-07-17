137eeda02d1b98c51ad85ff3682604e0b3307940
UnLuckyNikolay <nickslast@gmail.com>
2025-07-17 06:08:57 +0200

    Add draw_line to Window

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from window import Window
+from window import Window, Line, Point
 
 
 def main():
     window = Window(800, 600)
+    line1 = Line(Point(100, 100), Point(200, 200))
+    line2 = Line(Point(150, 200), Point(200, 200))
+    line3 = Line(Point(150, 200), Point(300, 150))
+    window.draw_line(line1, "black")
+    window.draw_line(line2, "gray")
+    window.draw_line(line3, "red")
     window.wait_for_close()
 
 
--- window.py
+++ window.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+
 from tkinter import Tk, BOTH, Canvas
 
 
 class Window:
-    def __init__(self, width, height):
+    def __init__(self, width : int, height : int):
         self._root = Tk() # Check args
         self._root.title("Maze Solver")
         self._canvas = Canvas(self._root, bg="white", height=height, width=width)
@@ -23,3 +25,23 @@ class Window:
 
     def close(self):
         self._running = False
+
+    def draw_line(self, line : Line, fill_color : str = "black"):
+        line.draw(self._canvas, fill_color=fill_color)
+
+class Point:
+    def __init__(self, x : int, y : int):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1 : Point, point2 : Point):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas : Canvas, fill_color : str):
+        canvas.create_line(
+            self.point1.x, self.point1.y,
+            self.point2.x, self.point2.y,
+            fill=fill_color, width=2
+        )
